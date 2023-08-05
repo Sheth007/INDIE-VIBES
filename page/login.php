ecbed4b00c65d38ddf1555c5910148efc0926014
Sheth Uday <shethuday8@gmail.com>
2023-08-05 22:58:51 +0530

    new files

---
+++ page/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Login -IV</title>
+     <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+     <div class="container">
+          <h1 class="title">Indie Vibes</h1>
+          <div class="card">
+              <form method="POST">
+                  <input type="text" placeholder="Email" name="email">
+                  <input type="password" placeholder="Password" name="password">
+                  <div class="buttons">
+                    <a href="../page/signup.php" class="register-link">Register</a>
+                      <button type="submit" class="login-button">Login</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+</body>
+</html>
---
+++ page/signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Register - IV</title>
+     <link rel="stylesheet" href="../css/register.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Indie Vibes - SignUp</h1>
+        <div class="card">
+            <form method="POST">
+            <input type="email" placeholder="Email" name="Email">
+                  <input type="password" placeholder="Password" name="Password">
+                  <input type="password" placeholder="Re-Password" name="Re-Password">
+                  <div class="buttons">
+                    <a href="../page/login.php" class="register-link">Login</a>
+                      <button type="submit" class="login-button">Register</button>
+                  </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
