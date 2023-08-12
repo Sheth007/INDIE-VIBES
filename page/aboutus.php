262f6491d5b86b3988e7c9b4d676ead20eb7397a
Sheth Uday <shethuday8@gmail.com>
2023-08-12 22:22:16 +0530

    Updated Files
    Database file is attached.....

---
+++ page/aboutus.php
@@ -0,0 +1,50 @@
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>About Us</title>
+<style>
+body {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: black;
+}
+
+.about-us {
+  width: 400px;
+  margin: 30 auto;
+  padding: 20px;
+  background-color: black;
+  /* border: 1px solid #ccc; */
+}
+
+h1 {
+  text-align: center;
+  color: white;
+}
+
+p {
+  margin-bottom: 10px;
+  color: white;
+}
+</style>
+</head>
+<body>
+<div class="about-us">
+  <h1>About Us</h1>
+   <p>
+    Indie Vibes is a music website dedicated to promoting independent music. We believe that independent music is the future of music, and we are committed to providing a platform for independent artists to share their music with the world.
+  </p>
+  <p>
+    We are a team of music lovers who are passionate about independent music. We believe that indie music is more than just music, it's a culture. It's a way of life. It's a community.
+  </p>
+  <p>
+    We are here to help you discover new indie music, connect with other indie music fans, and support independent artists. We hope you enjoy our website!
+  </p>
+</div>
+</body>
+</html>
+
+<?php include 'footer.php'; ?>
