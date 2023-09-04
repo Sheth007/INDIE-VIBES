8ac239d0dec7675ac99f69bc415a2e4e186e3404
Sheth Uday <shethuday8@gmail.com>
2023-09-04 12:09:37 +0530

    Updated Files

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<style>
+   /* Footer styles (adjust as needed) */
+footer {
+  background: #222;
+  color: white;
+  text-align: center;
+  padding: 10px 0;
+}
+</style>
+<body>
+    <center><p>Welcome Indie Vibes Admin</p></center>
+</body>
+</html>
