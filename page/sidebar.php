79f7a2b0edb15f0a859bea1bbb933350edd7479b
Sheth Uday <shethuday8@gmail.com>
2023-09-04 12:10:48 +0530

    Updated Files

---
+++ page/sidebar.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+<div class="sidebar">
+  <div class="logo">
+    <a href="#">
+      <img src="../img/logo.png" alt="Logo" />
+    </a>
+  </div>
+
+  <div class="navigation">
+    <ul>
+      <li>
+        <a href="#">
+          <span class="fa fa-home"></span>
+          <span>Home</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          <span class="fa fa-search"></span>
+          <span>Search</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="../page/aboutus.php">
+          <span class="fa fas fa-book"></span>
+          <span>About Us</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="navigation">
+    <ul>
+      <li>
+        <a href="../page/Contact.php">
+          <span class="fa fas fa-plus-square"></span>
+          <span>Contact Us</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="../page/feedback.php">
+          <span class="fa fas fa-heart"></span>
+          <span>Feedback Us</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <!-- <div class="policies">
+    <ul>
+      <li>
+        <a href="#">Cookies</a>
+      </li>
+      <li>
+        <a href="#">Privacy</a>
+      </li>
+    </ul>
+  </div> -->
+</div>
+</body>
+</html>
